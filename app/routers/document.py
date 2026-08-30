92ab5325b828acb0a56c7407bf5dfd7113e99fa7
Geethika Nagasri Tammineni <tamminenigeethika9@gmail.com>
2026-08-30 23:18:30 +0530

    feat: implement async pdf extraction and document upload endpoint

--- app/routers/document.py
+++ app/routers/document.py
@@ -1,16 +1,32 @@
 from typing import List
 from fastapi import APIRouter, UploadFile, File, Depends, HTTPException
+from pypdf import PdfReader
 from sqlalchemy.orm import Session
-from ..db.database import get_db
-from ..core.security import get_current_user
 
+from app.db.database import get_db
+from app.core.security import get_current_user
+from app.service.pdf_processor import extract_pdf_content
 
 router = APIRouter()
 
 @router.post("/upload")
-async def upload_documents(user_id =  Depends(get_current_user), files: List[UploadFile] = File(...), db: Session = Depends(get_db)):
-
+async def upload_documents(
+    user_id: str = Depends(get_current_user),
+    files: List[UploadFile] = File(...),
+    db: Session = Depends(get_db)
+):
     if not files:
         raise HTTPException(status_code=400, detail="No files uploaded.")
+        
+    print(f"Starting document processing: received {len(files)} file(s) from user {user_id}.")
 
-    print(f"Starting document processing: received {len(files)}.")
+    for file in files:
+        # Create your custom session ID
+        session_id = f"{file.filename}/{user_id}"
+        
+        # Load the PDF into memory
+        reader = PdfReader(file.file)
+        
+        full_text, file_sample = await extract_pdf_content(reader)
+        
+        return{"Full_text":full_text, "File_sample":file_sample}
