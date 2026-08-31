7f9ea04c3aa80c7733a483d16b57c7dfd5531e2a
Geethika Nagasri Tammineni <tamminenigeethika9@gmail.com>
2026-08-31 21:33:37 +0530

    refactor: integrate fastapi lifespan context manager for startup logic

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+
 from .db.database import engine, Base
-from .routers import auth
+from .routers import auth, document
 
-#Create all database tables based on your models.py
-Base.metadata.create_all(bind=engine)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup logic: Create all database tables based on your models.py
+    Base.metadata.create_all(bind=engine)
+    
+    yield 
 
-#Initialize the FastAPI application
-app = FastAPI()
+# Initialize the FastAPI application with the lifespan manager
+app = FastAPI(lifespan=lifespan)
 
-#Include  routers to connect the endpoints
+# Include routers to connect the endpoints
 app.include_router(auth.router)
+app.include_router(document.router) # Adjusts prefix if we use one
 
 @app.get("/")
 def health_check():
